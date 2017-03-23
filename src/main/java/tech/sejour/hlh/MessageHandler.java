06de4d2c771cbb5ec53a5fa4e389a449c3b4f5a9
Shuka Takakuma <shuka.takakuma@gmail.com>
2017-03-23 15:10:27 +0900

    fix

--- src/main/java/tech/sejour/hlh/MessageHandler.java
+++ src/main/java/tech/sejour/hlh/MessageHandler.java
@@ -4,15 +4,18 @@ import com.linecorp.bot.model.event.Event;
 import com.linecorp.bot.model.event.MessageEvent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
 import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 
 @LineMessageHandler
 public class MessageHandler {
 
+    @EventMapping
     public TextMessage handleTextMessage(MessageEvent<TextMessageContent> event) {
         return new TextMessage("> " + event.getMessage().getText());
     }
 
+    @EventMapping
     public TextMessage handleDefault(Event event) {
         return new TextMessage("hello " + event.getSource().getSenderId());
     }
