db0b1a07ca77e8c391bd9482dd5e8ffb8bfbb0dc
Shuka Takakuma <shuka.takakuma@gmail.com>
2017-04-01 05:30:37 +0900

    try

--- src/main/java/tech/sejour/hlh/configuration/AppConfig.java
+++ src/main/java/tech/sejour/hlh/configuration/AppConfig.java
@@ -1,9 +1,7 @@
 package tech.sejour.hlh.configuration;
 
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("tech.sejour.diamond")
 public class AppConfig {
 }
