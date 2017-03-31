bfba411e6315a4a97ed3761d1c3f738b0ebf3aa3
Shuka Takakuma <shuka.takakuma@gmail.com>
2017-04-01 03:02:40 +0900

    add component scan for diamond

---
+++ src/main/java/tech/sejour/hlh/configuration/AppConfig.java
@@ -0,0 +1,9 @@
+package tech.sejour.hlh.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("tech.sejour.diamond")
+public class AppConfig {
+}
